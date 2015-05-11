76a63110ff8177f8bc32e6bdb953869dabcc82ee
ArsenyNevsky <erafiil@gmail.com>
2015-05-11 20:13:21 +0300

    I've represent for common use the first version of merge sort

--- src/ru/nevars/Main.java
+++ src/ru/nevars/Main.java
@@ -1,31 +1,16 @@
 package ru.nevars;
 
-import ru.nevars.tree.AbstractTree;
-import ru.nevars.tree.BinaryTree;
+import ru.nevars.sort.AbstractSort;
+import ru.nevars.sort.ShellSort;
 
 public class Main {
 
     public static void main(String[] args) {
         //int array[] = {9,0,1,8,2,7,3,6,5,4};
         //int array[] = {3,3,3,3,4,2,0,1};
-        /*int array[] = {0,9,8,8,7,6,5,4,3,2,1};
+        int array[] = {0,9,8,8,7,6,5,4,3,2,1};
         AbstractSort sort = new ShellSort();
         sort.sort(array);
-        sort.printArray("Sorted array");*/
-
-        AbstractTree tree = new BinaryTree();
-        tree.add(8);
-        tree.add(3);
-        tree.add(10);
-        tree.add(1);
-        tree.add(6);
-        tree.add(14);
-        tree.add(4);
-        tree.add(7);
-        tree.add(13);
-        tree.add(-1);
-        tree.add(0);
-
-        tree.printTree();
+        sort.printArray("Sorted array");
     }
 }
--- src/ru/nevars/sort/MergeSort.java
+++ src/ru/nevars/sort/MergeSort.java
@@ -7,6 +7,48 @@ public class MergeSort extends AbstractSort {
 
     @Override
     void sort() {
+        mergeSort(array, 0, SIZE - 1);
+    }
 
+    private void mergeSort(int array[], int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int middle = (left + right) / 2;
+        mergeSort(array, 0, middle);
+        mergeSort(array, middle + 1, right);
+        merge(array, left, right);
+    }
+
+    // 1 3 5 6 2 4 7 8
+    public int[] merge(int array[], int left, int right) {
+        int temp[] = new int[right];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = array[i];
+        }
+
+        int ind = 0;
+        int i = left;
+        final int middle = (left + right) / 2;
+        int j = middle;
+
+        while (ind < right) {
+            if (i == middle) {
+                for (int k = j; k < right; k++) {
+                    array[ind++] = temp[k];
+                }
+            } else if (j == right) {
+                for (int k = i; k < middle; k++) {
+                    array[ind++] = temp[k];
+                }
+            } else if (temp[i] < temp[j] && i < middle && j < right) {
+                array[ind++] = temp[i];
+                i++;
+            } else if (temp[i] > temp[j] && j < right && i < middle) {
+                array[ind++] = temp[j];
+                j++;
+            }
+        }
+        return array;
     }
 }
